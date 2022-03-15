7f33a6533e65599a2f4592d185f2423ad6be6cb3
yeferman <yeysonqqc1gmail.com>
2022-03-15 13:46:48 -0500

    projects

---
+++ News/scrip.js
@@ -0,0 +1,16 @@
+
+let saludo = "Hola";
+function sumar(num1, num2,){
+  let suma = num1 + num2;
+
+  return suma;
+}
+
+
+let result = sumar(10,10);
+const PI = 3.1416;
+console.log(result +" " + PI);
+
+
+
+
